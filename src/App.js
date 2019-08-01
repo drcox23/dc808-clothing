7cb3de06db84b2f34c9fda0910088a484900893e
doug <drcox23@gmail.com>
2019-07-31 21:02:06 -1000

    created sign in component and sign in with google component

--- src/App.js
+++ src/App.js
@@ -7,23 +7,41 @@ import ShopPage from './pages/shop/Shop.component.jsx';
 import Header from './components/header/Header.component';
 import SignInAndOut from './pages/sign-in-and-out/Sign-in-and-out.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndOut} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndOut} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
